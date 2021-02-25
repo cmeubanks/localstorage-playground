b3f5f6d2250ab6494bb16227f98fe6cacfe2c711
Caroline Eubanks <cmeubanks@gmail.com>
2021-02-25 11:54:28 -0700

    display saved user info

--- scripts.js
+++ scripts.js
@@ -1,29 +1,29 @@
-/* Use this area for pseudo - coding:
-// need variables to capture button, and inputs for  name and e-Email
-//button event listeners
-//.value of inputs
-//store the values in an object
-//give object a key "logininfo"
-
-
-*/
 
 // Query selectors
-var nameBlah = document.querySelector('#name');
-var email = document.querySelector('#email');
 var addContactBtn = document.querySelector('#register-btn');
-
+var displayBtn = document.querySelector('#display-btn');
+var yourName = document.querySelector('#name');
+var email = document.querySelector('#email');
+var displayArea = document.querySelector('.display-area');
 
 
 
 // Event listeners
-addContactBtn.addEventListener('click', saveContactInfo);
+addContactBtn.addEventListener('click', storeInfo)
+displayBtn.addEventListener('click', getSavedInfo)
 
 
 
 // Functions
+function storeInfo() {
+  var login = {userName: yourName.value, userEmail: email.value};
+  var loginString = JSON.stringify(login);
+  localStorage.setItem('info', loginString);
+}
 
-function createObject() {
-var personalInfo =  { userName: nameBlah.value, userEmail: email.value };
-return personalInfo;
+function getSavedInfo() {
+  var retrieveLogin = localStorage.getItem('info');
+  var loginString = JSON.parse(retrieveLogin);
+  displayArea.innerText = `E-Mail:${loginString.userEmail} Username:${loginString.userName}`;
+
 }
